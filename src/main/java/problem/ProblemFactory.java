9d8f50f66b337452f1d2c9e9cf1524ccd0c3b144
Toadytoad <tom.philip.ca@gmail.com>
2023-05-23 13:17:29 -0400

    Added exponentiation problem

--- src/main/java/problem/ProblemFactory.java
+++ src/main/java/problem/ProblemFactory.java
@@ -25,14 +25,19 @@ public class ProblemFactory {
         int a;
         int b;
         if(type=='/'){
-            a = rand.nextInt((int)Math.pow(10, Math.floor((double)dif/2)));
-            b = rand.nextInt((int)Math.pow(10, Math.ceil((double)dif/2)));
-        } else {
-            a = rand.nextInt((int) Math.pow(10, dif));
-            b = rand.nextInt((int) Math.pow(10, dif));
+            a = rand.nextInt((int)Math.pow(10, Math.floor((double)dif/2)))+1;
+            b = rand.nextInt((int)Math.pow(10, Math.ceil((double)dif/2)))+1;
+        } else if(type=='^') {
+            b = rand.nextInt(3)+2;
+            a = rand.nextInt((int)Math.pow(10, (double)dif/b))+2;
+        }
+        else {
+            a = rand.nextInt((int) Math.pow(10, dif))+1;
+            b = rand.nextInt((int) Math.pow(10, dif))+1;
         }
         int ans;
         String op;
+        String suf = "";
         if(type == '+'){
             ans = a+b;
             op = "+";
@@ -49,6 +54,10 @@ public class ProblemFactory {
             ans = a;
             a = ans*b;
             op="\\div";
+        } else if(type =='^'){
+            ans = (int) Math.pow(a, b);
+            op = "^{";
+            suf = "}";
         }
         else {
             throw new IllegalArgumentException();
@@ -56,6 +65,8 @@ public class ProblemFactory {
         question.append(a);
         question.append(op);
         question.append(b);
+        question.append(suf);
         return new Problem(question.toString(), new int[]{ans});
     }
+
 }
